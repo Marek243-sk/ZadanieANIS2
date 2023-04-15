cf2b9f994b31e86fa0482262e173f7cd42ecd50d
Marek Puškáš <113102926+Marek243-sk@users.noreply.github.com>
2023-04-15 13:00:51 +0200

    Builder triedy Letenka, rozhranie EvidenciaLetenky.

---
+++ src/zadanie/EvidenciaLetenky.java
@@ -0,0 +1,18 @@
+package zadanie;
+
+public interface EvidenciaLetenky {
+    public String getCisloSedadla();
+    public void setCisloSedadla(String cisloSedadla);
+    public String getTrieda();
+    public void setTrieda(String trieda);
+    public int getCena();
+    public void setCena(int cena);
+    public String getDruh();
+    public void setDruh(String druh);
+    public String getMiestoOdletu();
+    public void setMiestoOdletu(String miestoOdletu);
+    public String getMiestoPriletu();
+    public void setMiestoPriletu(String miestoPriletu);
+    public String getDatum();
+    public void setDatum(String datum);
+}
--- src/zadanie/Letenka.java
+++ src/zadanie/Letenka.java
@@ -1,11 +1,122 @@
 package zadanie;
 
-public class Letenka {
-    public void objednajLetenku() {
+public class Letenka implements EvidenciaLetenky{
+    private String cisloSedadla;
+    private String trieda;
+    private int cena;
+    private String druh;
+    private String miestoOdletu;
+    private String miestoPriletu;
+    private String datum;
 
+    private Letenka(LetenkaBuilder builder) {
+        this.cisloSedadla = builder.cisloSedadla;
+        this.trieda = builder.trieda;
+        this.cena = builder.cena;
+        this.druh = builder.druh;
+        this.miestoOdletu = builder.miestoOdletu;
+        this.miestoPriletu = builder.miestoPriletu;
+        this.datum = builder.datum;
     }
 
-    public void vratLetenku() {
+    public String getCisloSedadla() {
+        return cisloSedadla;
+    }
 
+    public void setCisloSedadla(String cisloSedadla) {
+        this.cisloSedadla = cisloSedadla;
+    }
+
+    public String getTrieda() {
+        return trieda;
+    }
+
+    public void setTrieda(String trieda) {
+        this.trieda = trieda;
+    }
+
+    public int getCena() {
+        return cena;
+    }
+
+    public void setCena(int cena) {
+        this.cena = cena;
+    }
+
+    public String getDruh() {
+        return druh;
+    }
+
+    public void setDruh(String druh) {
+        this.druh = druh;
+    }
+
+    public String getMiestoOdletu() {
+        return miestoOdletu;
+    }
+
+    public void setMiestoOdletu(String miestoOdletu) {
+        this.miestoOdletu = miestoOdletu;
+    }
+
+    public String getMiestoPriletu() {
+        return miestoPriletu;
+    }
+
+    public void setMiestoPriletu(String miestoPriletu) {
+        this.miestoPriletu = miestoPriletu;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public static class LetenkaBuilder {
+        private String cisloSedadla;
+        private String trieda;
+        private int cena;
+        private String druh;
+        private String miestoOdletu;
+        private String miestoPriletu;
+        private String datum;
+
+        public LetenkaBuilder(String cisloSedadla, String trieda) {     /*musi byt*/
+            this.cisloSedadla = cisloSedadla;
+            this.trieda = trieda;
+        }
+
+        public  LetenkaBuilder setCena(int cena) {
+            this.cena = cena;
+            return this;
+        }
+
+        public LetenkaBuilder setDruh(String druh) {
+            this.druh = druh;
+            return this;
+        }
+
+        public LetenkaBuilder setMiestoOdletu(String miestoOdletu) {
+            this.miestoOdletu = miestoOdletu;
+            return this;
+        }
+
+        public LetenkaBuilder setMiestoPriletu(String miestoPriletu) {
+            this.miestoPriletu = miestoPriletu;
+            return this;
+        }
+
+        public LetenkaBuilder setDatum(String datum) {
+            this.datum = datum;
+            return this;
+        }
+
+        public Letenka vytvor() {
+            Letenka letenka = new Letenka(this);
+            return letenka;
+        }
     }
 }
